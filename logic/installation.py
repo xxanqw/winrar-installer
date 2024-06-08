337a77e2fea24cb760c2c5efcbb39d7083432dfb
Іван Потієнко <potik217@gmail.com>
2024-06-09 02:04:02 +0300

    Fixed launch after install and new funcs for lang and datemodified

--- logic/installation.py
+++ logic/installation.py
@@ -3,6 +3,7 @@ from PyQt6.QtCore import QThread
 from PyQt6.QtWidgets import QApplication
 from subprocess import run
 import subprocess
+from os import path as p
 
 class InstallerThread(QThread):
     def __init__(self, path_to_winrar_installer):
@@ -13,9 +14,11 @@ class InstallerThread(QThread):
         run([self.path_to_winrar_installer, '/S'], shell=True, check=True)
         print("Installation completed!")
 
-def launch_winrar(self, arch):
+def launch_winrar(arch):
     if arch == "x32":
         path_to_winrar_executable = "C:\\Program Files (x86)\\WinRAR\\WinRAR.exe"
+        if not p.exists(path_to_winrar_executable):
+            path_to_winrar_executable = "C:\\Program Files\\WinRAR\\WinRAR.exe"
     elif arch == "x64":
         path_to_winrar_executable = "C:\\Program Files\\WinRAR\\WinRAR.exe"
         
--- logic/version.py
+++ logic/version.py
@@ -1,6 +1,5 @@
 import requests
 
-# URL of the JSON file
 url = "https://fs.xserv.pp.ua/files/winrar.json"
 response = requests.get(url)
 
@@ -8,8 +7,28 @@ def get_versions():
     if response.status_code == 200:
         versions = response.json()
         version_list = versions["versions"]
-        print("Successfully retrieved versions from the JSON file.")
+        print("Successfully retrieved versions.")
 
         return version_list
     else:
-        print("Failed to retrieve versions from the JSON file.")
+        print("Failed to retrieve versions.")
+
+def get_languages():
+    if response.status_code == 200:
+        languages = response.json()
+        language_list = languages["languages"]
+        print("Successfully retrieved languages.")
+
+        return language_list
+    else:
+        print("Failed to retrieve languages.")
+
+def get_lastmod():
+    if response.status_code == 200:
+        lastmod = response.json()
+        lastmod_date = lastmod["datemodified"]
+        print("Successfully retrieved last modified date.")
+
+        return lastmod_date
+    else:
+        print("Failed to retrieve last modified date.")
